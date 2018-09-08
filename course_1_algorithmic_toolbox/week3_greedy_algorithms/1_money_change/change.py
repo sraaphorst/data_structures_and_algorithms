0363495093cb38a61ce53571bf12f330164eaff7
sraaphorst <sraaphorst@gemini.edu>
2018-09-08 19:44:05 -0300

    Cleanup and starting week 3.

--- course_1_algorithmic_toolbox/week3_greedy_algorithms/1_money_change/change.py
+++ course_1_algorithmic_toolbox/week3_greedy_algorithms/1_money_change/change.py
@@ -1,10 +1,18 @@
 # Uses python3
 import sys
+import unittest
 
 def get_change(m):
-    #write your code here
+    return m // 10 + (m % 10) // 5 + m % 5
     return m
 
+class TestChange(unittest.TestCase):
+    def test_2(self):
+        self.assertEqual(get_change(2), 2)
+    def test_28(self):
+        self.assertEqual(get_change(28), 6)
+
 if __name__ == '__main__':
+    #unittest.main()
     m = int(sys.stdin.read())
     print(get_change(m))
