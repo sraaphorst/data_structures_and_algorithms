b9269a86c67ad96fd580a99b01cbb968ff7df3d6
Sebastian Raaphorst <srcoding@gmail.com>
2018-09-22 18:17:02 -0300

    Finished course 2 week 3.

--- course_2_data_structures/week3_priority_queues_and_disjoint_sets/merging_tables/merging_tables.cpp
+++ course_2_data_structures/week3_priority_queues_and_disjoint_sets/merging_tables/merging_tables.cpp
@@ -1,65 +1,80 @@
+/**
+ * merging_tables.cpp
+ *
+ * By Sebastian Raaphorst, 2018.
+ *
+ * This is supposed to be a simulation of merging database tables.
+ * I found the instructions very unclear, but as it simply entailed a basic implementation of disjoint sets,
+ * it appears that I am solving the problem that I don't quite understand.
+ */
+
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
 #include <algorithm>
 #include <iostream>
 
-using std::cin;
-using std::cout;
-using std::endl;
-using std::max;
-using std::vector;
 
 struct DisjointSetsElement {
-	int size, parent, rank;
+	size_t size, rank;
+	size_t parent;
 	
-	DisjointSetsElement(int size = 0, int parent = -1, int rank = 0):
+	explicit DisjointSetsElement(size_t size = 0, size_t parent = 0, size_t rank = 0):
 	    size(size), parent(parent), rank(rank) {}
 };
 
 struct DisjointSets {
-	int size;
-	int max_table_size;
-	vector <DisjointSetsElement> sets;
+	size_t size;
+	size_t max_table_size;
+	std::vector <DisjointSetsElement> sets;
 
-	DisjointSets(int size): size(size), max_table_size(0), sets(size) {
-		for (int i = 0; i < size; i++)
+	explicit DisjointSets(size_t size): size(size), max_table_size(0), sets(size) {
+		for (size_t i = 0; i < size; i++)
 			sets[i].parent = i;
 	}
 
-	int getParent(int table) {
-		// find parent and compress path
+	size_t getParent(size_t table) {
+		if (sets[table].parent != table)
+			sets[table].parent = getParent(sets[table].parent);
+		return sets[table].parent;
 	}
 
-	void merge(int destination, int source) {
-		int realDestination = getParent(destination);
-		int realSource = getParent(source);
+	void merge(size_t destination, size_t source) {
+		size_t realDestination = getParent(destination);
+		size_t realSource = getParent(source);
 		if (realDestination != realSource) {
-			// merge two components
-			// use union by rank heuristic
-                        // update max_table_size
-		}		
+			if (sets[realDestination].rank > sets[realSource].rank) {
+                sets[realSource].parent = realDestination;
+                sets[realDestination].size += sets[realSource].size;
+            } else {
+				sets[realDestination].parent = realSource;
+				sets[realSource].size += sets[realDestination].size;
+				if (sets[realDestination].rank == sets[realSource].rank)
+					++sets[realSource].rank;
+			}
+		}
+        max_table_size = std::max(max_table_size, std::max(sets[realDestination].size, sets[realSource].size));
 	}
 };
 
 int main() {
-	int n, m;
-	cin >> n >> m;
+	size_t n, m;
+	std::cin >> n >> m;
 
 	DisjointSets tables(n);
 	for (auto &table : tables.sets) {
-		cin >> table.size;
-		tables.max_table_size = max(tables.max_table_size, table.size);
+		std::cin >> table.size;
+		tables.max_table_size = std::max(tables.max_table_size, table.size);
 	}
 
-	for (int i = 0; i < m; i++) {
-		int destination, source;
-		cin >> destination >> source;
+	for (size_t i = 0; i < m; i++) {
+		size_t destination, source;
+		std::cin >> destination >> source;
                 --destination;
                 --source;
 		
 		tables.merge(destination, source);
-	        cout << tables.max_table_size << endl;
+	        std::cout << tables.max_table_size << std::endl;
 	}
 
 	return 0;
