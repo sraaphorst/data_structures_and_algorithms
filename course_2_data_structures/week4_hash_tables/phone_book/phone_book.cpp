c6677ca29f4b3ab8830e743f2f42386c9f350d95
sraaphorst <sraaphorst@gemini.edu>
2018-09-24 16:13:14 -0300

    Working on C2W4.

--- course_2_data_structures/week4_hash_tables/phone_book/phone_book.cpp
+++ course_2_data_structures/week4_hash_tables/phone_book/phone_book.cpp
@@ -1,69 +1,110 @@
+/**
+ * phone_book.cpp
+ *
+ * By Sebastian Raaphorst, 2018.
+ *
+ * Direct addressing implementation of a phone book.
+ */
+
+#include <algorithm>
 #include <iostream>
+#include <iterator>
 #include <vector>
 #include <string>
 
-using std::string;
-using std::vector;
-using std::cin;
+using phone_name = std::string;
+using phone_number = size_t;
 
-struct Query {
-    string type, name;
-    int number;
+enum class Request {
+    add,
+    del,
+    find,
 };
 
-vector<Query> read_queries() {
-    int n;
-    cin >> n;
-    vector<Query> queries(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> queries[i].type;
-        if (queries[i].type == "add")
-            cin >> queries[i].number >> queries[i].name;
-        else
-            cin >> queries[i].number;
+struct Query {
+    Request type;
+    phone_name name;
+    phone_number number;
+};
+
+
+class PhoneBook {
+    constexpr static const size_t num_entries = 10'000'000;
+    std::string *entries;
+
+public:
+    PhoneBook() {
+        entries = new std::string[num_entries];
+        std::for_each(entries, entries + num_entries, [](auto &entry) { entry = ""; });
+    }
+
+    PhoneBook(const PhoneBook &) = delete;
+
+    PhoneBook(PhoneBook &&) = delete;
+
+    PhoneBook &operator=(const PhoneBook &) = delete;
+
+    ~PhoneBook() {
+        delete[] entries;
+    }
+
+    std::string &operator[](size_t idx) {
+        return entries[idx];
+    }
+
+    const std::string &operator[](size_t idx) const {
+        return entries[idx];
+    }
+};
+
+
+std::vector<Query> read_queries() {
+    // Number of operations.
+    size_t n;
+    std::cin >> n;
+
+    std::vector<Query> queries(n);
+    for (auto &query: queries) {
+        std::string type_name;
+        std::cin >> type_name;
+        if (type_name == "add") {
+            query.type = Request::add;
+            std::cin >> query.number >> query.name;
+        } else if (type_name == "del") {
+            query.type = Request::del;
+            std::cin >> query.number;
+        } else {
+            query.type = Request::find;
+            std::cin >> query.number;
+        }
     }
     return queries;
 }
 
-void write_responses(const vector<string>& result) {
-    for (size_t i = 0; i < result.size(); ++i)
-        std::cout << result[i] << "\n";
+std::vector<std::string> process_queries(const std::vector<Query> &queries) {
+    PhoneBook book;
+    std::vector<std::string> result;
+
+    // Keep list of all existing (i.e. not deleted yet) contacts.
+    std::vector<Query> contacts;
+    for (const auto &query: queries) {
+        switch (query.type) {
+            case Request::add:
+                book[query.number] = query.name;
+                break;
+            case Request::del:
+                book[query.number] = "";
+                break;
+            case Request::find:
+                result.emplace_back(book[query.number].empty() ? "not found" : book[query.number]);
+                break;
+        };
+    }
+    return result;
 }
 
-vector<string> process_queries(const vector<Query>& queries) {
-    vector<string> result;
-    // Keep list of all existing (i.e. not deleted yet) contacts.
-    vector<Query> contacts;
-    for (size_t i = 0; i < queries.size(); ++i)
-        if (queries[i].type == "add") {
-            bool was_founded = false;
-            // if we already have contact with such number,
-            // we should rewrite contact's name
-            for (size_t j = 0; j < contacts.size(); ++j)
-                if (contacts[j].number == queries[i].number) {
-                    contacts[j].name = queries[i].name;
-                    was_founded = true;
-                    break;
-                }
-            // otherwise, just add it
-            if (!was_founded)
-                contacts.push_back(queries[i]);
-        } else if (queries[i].type == "del") {
-            for (size_t j = 0; j < contacts.size(); ++j)
-                if (contacts[j].number == queries[i].number) {
-                    contacts.erase(contacts.begin() + j);
-                    break;
-                }
-        } else {
-            string response = "not found";
-            for (size_t j = 0; j < contacts.size(); ++j)
-                if (contacts[j].number == queries[i].number) {
-                    response = contacts[j].name;
-                    break;
-                }
-            result.push_back(response);
-        }
-    return result;
+void write_responses(const std::vector<std::string> &result) {
+    std::copy(std::cbegin(result), std::cend(result), std::ostream_iterator<std::string>(std::cout, "\n"));
 }
 
 int main() {
