3a1a1375800abeeb515c821e821be49e4ca32fc6
sraaphorst <sraaphorst@gemini.edu>
2018-09-24 18:37:18 -0300

    C2W4E2.

--- course_2_data_structures/week4_hash_tables/hash_chains/hash_chains.cpp
+++ course_2_data_structures/week4_hash_tables/hash_chains/hash_chains.cpp
@@ -1,81 +1,105 @@
+/**
+ * hash_chains.cpp
+ *
+ * By Sebastian Raaphorst, 2018.
+ *
+ * Basic set implementation using hashing with a specified number of buckets.
+ */
+
 #include <iostream>
+#include <iterator>
 #include <string>
 #include <vector>
 #include <algorithm>
 
-using std::string;
-using std::vector;
-using std::cin;
+class QueryProcessor final {
+    using data = std::string;
+    using hashchain = std::vector<data>;
+    using hashtable = std::vector<hashchain>;
 
-struct Query {
-    string type, s;
-    size_t ind;
-};
+    const size_t bucket_count;
 
-class QueryProcessor {
-    int bucket_count;
-    // store all strings in one vector
-    vector<string> elems;
-    size_t hash_func(const string& s) const {
-        static const size_t multiplier = 263;
-        static const size_t prime = 1000000007;
+    // The hashtable, consisting of bucket_count vectors of string.
+    hashtable table;
+
+    // Calculate the hash of the data.
+    size_t hash_func(const data &s) const {
+        constexpr static const size_t multiplier = 263;
+        constexpr static const size_t prime = 1'000'000'007;
+
+        // Iterate over the string in reverse.
         unsigned long long hash = 0;
-        for (int i = static_cast<int> (s.size()) - 1; i >= 0; --i)
-            hash = (hash * multiplier + s[i]) % prime;
-        return hash % bucket_count;
+        for (auto iter = std::rbegin(s); iter != std::rend(s); ++iter)
+            hash = (hash * multiplier + *iter) % prime;
+
+        // We want a positive value.
+        return ((hash % bucket_count) + bucket_count) % bucket_count;
     }
 
 public:
-    explicit QueryProcessor(int bucket_count): bucket_count(bucket_count) {}
+    explicit QueryProcessor(size_t bucket_count):
+        bucket_count(bucket_count),
+        table{hashtable(bucket_count)} {}
 
-    Query readQuery() const {
-        Query query;
-        cin >> query.type;
-        if (query.type != "check")
-            cin >> query.s;
-        else
-            cin >> query.ind;
-        return query;
-    }
 
-    void writeSearchResult(bool was_found) const {
-        std::cout << (was_found ? "yes\n" : "no\n");
-    }
+    // Process a query, depending on type.
+    void readAndExecuteQuery(std::istream &in, std::ostream &out) {
+        // Fetch the query type.
+        std::string query_type;
+        in >> query_type;
 
-    void processQuery(const Query& query) {
-        if (query.type == "check") {
-            // use reverse order, because we append strings to the end
-            for (int i = static_cast<int>(elems.size()) - 1; i >= 0; --i)
-                if (hash_func(elems[i]) == query.ind)
-                    std::cout << elems[i] << " ";
-            std::cout << "\n";
-        } else {
-            vector<string>::iterator it = std::find(elems.begin(), elems.end(), query.s);
-            if (query.type == "find")
-                writeSearchResult(it != elems.end());
-            else if (query.type == "add") {
-                if (it == elems.end())
-                    elems.push_back(query.s);
-            } else if (query.type == "del") {
-                if (it != elems.end())
-                    elems.erase(it);
-            }
+        /*** CHECK QUERY ***/
+        if (query_type == "check") {
+            // Read the row of the table to output.
+            size_t index;
+            in >> index;
+
+            std::copy(std::crbegin(table[index]), std::crend(table[index]), std::ostream_iterator<data>(out, " "));
+            out << '\n';
+            return;
         }
-    }
 
-    void processQueries() {
-        int n;
-        cin >> n;
-        for (int i = 0; i < n; ++i)
-            processQuery(readQuery());
+        // Any of the other queries rely on a key, so get:
+        // 1. The chain that would contain this key (i.e. the chain index to which it would hash); and
+        // 2. An iterator to the key, if one exists.
+        data key;
+        in >> key;
+        auto &chain = table[hash_func(key)];
+        const auto iter = std::find(std::cbegin(chain), std::cend(chain), key);
+
+        /*** FIND ***/
+        if (query_type == "find") {
+            out << (iter == std::cend(chain) ? "no" : "yes") << '\n';
+            return;
+        }
+
+        /*** ADD ***/
+        if (query_type == "add") {
+            if (iter == std::cend(chain))
+                chain.emplace_back(key);
+            return;
+        }
+
+        /*** DEL ***/
+        if (query_type == "del") {
+            if (iter != std::cend(chain))
+                chain.erase(iter);
+            return;
+        }
     }
 };
 
 int main() {
-    std::ios_base::sync_with_stdio(false);
-    int bucket_count;
-    cin >> bucket_count;
+    // Determine the number of buckets and create the query processor.
+    size_t bucket_count;
+    std::cin >> bucket_count;
     QueryProcessor proc(bucket_count);
-    proc.processQueries();
+
+    // Determine the number of queries and process them one-by-one.
+    size_t n;
+    std::cin >> n;
+    for (auto i = 0; i < n; ++i)
+        proc.readAndExecuteQuery(std::cin, std::cout);
+
     return 0;
 }
